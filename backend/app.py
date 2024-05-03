9d1adf70d2f351325a9b29b87064d782787d38a9
raefknani <raef.knani@gmail.com>
2024-05-03 12:52:58 +0100

    version 1 filre

--- backend/app.py
+++ backend/app.py
@@ -20,11 +20,94 @@ def send_report(path):
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    # Get the user's input
-    user_input = request.form.get('description', '')
-    UserInput = user_input.lower()
+    user_input = request.form.get('description', '').lower() + " "
+    print("user :",user_input)
 
-    laptop_scores = {}
+    def combine_spects(animator_func, user_input):
+        student_spects = animator_func(user_input)
+        return student_spects
+    
+
+    def student_spects(txt):
+        if "etudiant" in txt:
+            return ["core i5","Ryzen 5","8GB"," 256GB","13","15","Windows 10","mac","apple","SSD"]
+        else:
+            return []    
+    def teacher_spects(txt):
+        if "professeur" in txt:
+            return ["core i5","Ryzen 5","8GB"," 256GB","13","15","Windows 10","mac","apple","SSD"]
+        else:
+            return []
+    def software_engineer_spects(txt):
+        if "software" in txt:
+            return ["core i5","Ryzen 5","8GB"," 256GB","13","15","Windows 10","mac","apple","SSD"]
+        else:
+            return []
+    def doctor_spects(txt):
+        if "docteur" in txt:
+            return ["core i5","Ryzen 5","8GB"," 256GB","13","15","Windows 10","mac","apple","SSD"]
+        else:
+            return []
+    def graphic_designer_spects(txt):
+        if "designer" in txt:
+            return ["Core i7","Ryzen 7","16GB"," 512GB","15","17","Windows 10","mac","apple","SSD"]
+        else:
+            return []
+    def architect_spects(txt):
+        if "architect" in txt:
+            return ["Core i7","Ryzen 7","16GB"," 512GB","15","17","Windows 10","mac","apple","SSD"]
+        else:
+            return []
+    def composer_spects(txt):
+        if "composer" in txt:
+            return ["Core i7","Ryzen 7","16GB","1TB","15","17","Windows 10","mac","apple","SSD"]
+        else:
+            return []
+    def gamer_spects(txt):
+        if "docteur" in txt:
+            return ["Core i7","Ryzen 7","16GB"," 512GB","15","17","Windows 10","SSD","GTX 1660","RTX 2060","RTX 2070","RTX 2080"]
+        else:
+            return []
+    def animator_spects(txt):   
+        if "animator" in txt:
+            return ["Core i7","Ryzen 7","32GB"," 512GB","15","17","mac","apple","Windows 10","SSD","GTX 1660","RTX 2060","RTX 2070","RTX 2080"]
+        else:
+            return []
+    def Photographer_spects(txt):   
+        if "photo" or "photgrapheur" in txt:
+            return ["Core i7","Ryzen 7","16GB","1TB","15","17","mac","apple","Windows 10","SSD","GTX 1660","RTX 2060","RTX 2070","RTX 2080"]
+        else:
+            return []
+        
+
+    
+    final_spects = (combine_spects(student_spects, user_input)+
+                    combine_spects(teacher_spects, user_input)+
+                    combine_spects(software_engineer_spects, user_input)+
+                    combine_spects(doctor_spects, user_input)+ 
+                    combine_spects(graphic_designer_spects, user_input)+
+                    combine_spects(architect_spects, user_input)+ 
+                    combine_spects(composer_spects, user_input) +
+                    combine_spects(gamer_spects, user_input)  +
+                    combine_spects(animator_spects, user_input) +
+                    combine_spects(Photographer_spects, user_input) 
+                    )    
+    print("final_spects :",final_spects)
+
+
+
+    def text_finale(final_spects):
+        textfinale = ""
+        for i in range(len(final_spects)):
+            textfinale += final_spects[i] + " "
+        return textfinale
+   
+    user_input = text_finale(final_spects)
+    user_input = set(user_input.split())
+
+ 
+    print(student_spects(final_spects))
+    print("final_user_input :",user_input)
     checked_laptop = []
 
     # Load the JSON data
@@ -32,9 +115,6 @@ def submit():
         data = json.load(f)
     for laptop in data['laptops']:
         max = 0
-        UserInput = user_input.lower()
-        print(UserInput)
-        
         laptop_check = [
             laptop['marque'].lower(),
             laptop['processeur']['marque_processeur'].lower(),
@@ -43,11 +123,11 @@ def submit():
             laptop['stokage'].lower(),
             laptop['taille_ecran'].lower(),
             laptop['os'].lower(),
-            ]
-        print(laptop_check)
+        ]
+
         
         for i in range(len(laptop_check)):
-            if (laptop_check[i] in UserInput):
+            if (laptop_check[i] in user_input):
                 max += 1
             if max > 0:
                 checked_laptop.append(laptop["id"])
@@ -60,6 +140,7 @@ def submit():
    
 
     return json.dumps(checked_laptop)
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
